7387e0f04f05f33c5e8caefdcc4cf1f1d6b930f7
EdvardA1 <edvard.aldor@gmail.com>
2022-01-27 17:05:16 +0100

    Added files:src/main/java and test/java/DecideTest

---
+++ src/main/java/Decide.java
@@ -0,0 +1,160 @@
+package src.main.java;
+
+import java.awt.geom.Point2D;
+
+
+class Decide {
+    //Program for deciding hypothetical anti-ballistic missile system
+    //Call method decide with appropriate input to decide to send an interceptor or not.
+	
+	
+	//Global variables
+	int NUMPOINTS = 3;
+	double[] X = {0.23, 6.51, 0.15};
+	double[] Y = {1.31, 2.20, 1.3};
+	double LENGTH1 = 2.0;
+	double AREA1 = 6.6;
+	int PI = 180;
+	int EPSILON = 90;
+
+    //LIC conditions
+    public boolean LIC0(double[] X, double[] Y, int numpoints, double length1) {
+		double x1, y1, x2, y2, dist;
+		for(int i = 1; i < numpoints; i++){
+			x1 = X[i-1];
+			y1 = Y[i-1];
+			x2 = X[i];
+			y2 = Y[i];
+			dist = Point2D.distance(x1,y1,x2,y2);
+			if(dist > length1){return true;}
+		}
+		return false;
+	}
+
+	public boolean LIC1() {
+		return false;
+	}
+
+	public boolean LIC2() {
+		double x1, y1, x2, y2, x3, y3, angle;
+		for(int i = 2; i < NUMPOINTS; i++){
+			x1 = X[i-2];
+			y1 = Y[i-2];
+			x2 = X[i-1];
+			y2 = Y[i-1];
+			x3 = X[i];
+			y3 = Y[i];
+			
+			angle = Math.toDegrees((Math.atan2(y1-y2, x1-x2) - Math.atan2(y3-y2, x3-x2)));
+			
+			if (angle < 0) {
+				angle += 360;
+			}
+			if (angle > 180) {
+				angle = 360-angle;
+			}
+			
+			//System.out.println(angle);
+			
+			//If P1 or P3 is equal to P2 there is no angle
+			if (!((x1 == x2 && y1 == y2) || (x3 == x2 && y3 == y2))) {
+				if(angle < (PI-EPSILON) || angle > PI+EPSILON ){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public boolean LIC3() {
+		double x1, y1, x2, y2, x3, y3, area;
+		for(int i = 2; i < NUMPOINTS; i++){
+			x1 = X[i-2];
+			y1 = Y[i-2];
+			x2 = X[i-1];
+			y2 = Y[i-1];
+			x3 = X[i];
+			y3 = Y[i];
+			area = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0;
+			
+			//System.out.println(area);
+			
+			if(area > AREA1){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean LIC4() {
+		return false;
+	}
+
+	public boolean LIC5() {
+		return false;
+	}
+
+	public boolean LIC6() {
+		return false;
+	}
+
+	public boolean LIC7() {
+		return false;
+	}
+
+	public boolean LIC8() {
+		return false;
+	}
+
+	public boolean LIC9() {
+		return false;
+	}
+
+	public boolean LIC10() {
+		return false;
+	}
+
+	public boolean LIC11() {
+		return false;
+	}
+
+	public boolean LIC12() {
+		return false;
+	}
+
+	public boolean LIC13() {
+		return false;
+	}
+
+	public boolean LIC14() {
+		return false;
+	}
+	
+	
+    //conditions met vector
+    public int[] CMV() {
+		//computes the conditions met vector from the LIC's
+		int[] vector = {0};
+        return vector;
+    }
+    
+    //Preliminary unlock matrix
+    public int[][] PMV() {
+		//computes the PUM matrix from the CMV and the LCM
+		int[][] matrix = { {0} };
+        return matrix;
+	}
+	
+	//Final unlock vector
+	public int[] FUV() {
+		//computes the FUV vector from the PMV and the PUV
+		int[] vector = {0};
+        return vector;
+	}
+	
+    
+    //decide method
+    public void decide(int numpoints, int[] points, int[][] LCM, int[] PUV) {
+		//decides method, calls CMV, PUM, FUV and decides if it launches or not
+	}
+}
---
+++ src/test/java/DecideTest.java
@@ -0,0 +1,82 @@
+package src.test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecideTest {
+
+    @org.junit.jupiter.api.Test
+    void LIC0() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC4() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC6() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC7() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC8() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC9() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC10() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC11() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC12() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC13() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void LIC14() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void CMV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void PMV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void FUV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decide() {
+    }
+}
