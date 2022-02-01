56fc2ad6b664e7059d7498fff0376fd0473556a6
theanot <theanot@kth.se>
2022-02-01 15:34:31 +0100

    added unit tets for LIC11

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -13,5 +13,54 @@ class DecideTest {
 
     }
 
+	@Test
+	void LIC11FalseInputTest(){
+		int[][] points = new int[2][5];
+		points[0][0] = -2;
+		points[1][0] = 3;
+		points[0][1] = -1;
+		points[1][1] = 2;
+		Parameters parameters = new Parameters(0.0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0);
+		var dec = new Decide(parameters, 2, points);
+		assertFalse(dec.LIC11());
+	}
+	@Test
+	void LIC11FalseTest(){
+		int[][] points = new int[2][5];
+		points[0][0] = 1;
+		points[1][0] = 3;
+		points[0][1] = 2;
+		points[1][1] = 3;
+		points[0][2] = 3;
+		points[1][2] = 3;
+		points[0][3] = 4;
+		points[1][3] = 3;
+		points[0][4] = 5;
+		points[1][4] = 3;
+		Parameters parameters = new Parameters(0.0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0);
+		var dec = new Decide(parameters, 5, points);
+		assertFalse(dec.LIC4());
+	}
+	@Test
+	void LIC11TrueTest(){
+		int[][] points = new int[2][7];
+		points[0][0] = 1;
+		points[1][0] = 3;
+		points[0][1] = 2;
+		points[1][1] = 3;
+		points[0][2] = 3;
+		points[1][2] = 3;
+		points[0][3] = 4;
+		points[1][3] = 3;
+		points[0][4] = 5;
+		points[1][4] = 3;
+		points[0][5] = 56;
+		points[1][5] = 3;
+		points[0][6] = 5;
+		points[1][6] = 3;
+		Parameters parameters = new Parameters(0.0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0);
+		var dec = new Decide(parameters, 5, points);
+		assertTrue(dec.LIC4());
+	}
 
 }
