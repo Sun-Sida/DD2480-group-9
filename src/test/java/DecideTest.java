47db40ea00c6a7640975d5e2e2cabaaec98a7c9a
OscarIngels <oscar.ingels@gmail.com>
2022-02-01 15:03:04 +0100

    Two test cases added for LIC 12. One asserting false and one asserting true.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -10,6 +10,28 @@ class DecideTest {
         //assertFalse(dec.LIC0(dec.X, dec.Y, dec.NUMPOINTS,dec.LENGTH1));
 
     }
+    @Test
+    void LIC12TrueTest(){
+        var dec = new Decide();
+        double[] LIC12X = {2.0, 2.0, 5.0, 7.0, 8.0, 11.0};
+        double[] LIC12Y = {2.0, 5.0, 6.0, 4.0, 8.0, 4.0};
+        double LIC12Length1 = 4.0;
+        double LIC12Length2 = 7.0;
+        int LIC12K_pts = 2;
+        int LIC12Numpoints = 6;
+        assertTrue(dec.LIC12(LIC12X, LIC12Y,LIC12Numpoints,  LIC12Length1, LIC12Length2, LIC12K_pts));
+    }
+    @Test
+    void LIC12FalseTest(){
+        var dec = new Decide();
+        double[] LIC12X = {2.0, 2.0, 5.0, 7.0, 8.0, 11.0};
+        double[] LIC12Y = {2.0, 5.0, 6.0, 4.0, 8.0, 4.0};
+        double LIC12Length1 = 40.0;
+        double LIC12Length2 = 7.0;
+        int LIC12K_pts = 0;
+        int LIC12Numpoints = 6;
+        assertFalse(dec.LIC12(LIC12X, LIC12Y,LIC12Numpoints,  LIC12Length1, LIC12Length2, LIC12K_pts));
+    }
 
 
 }
