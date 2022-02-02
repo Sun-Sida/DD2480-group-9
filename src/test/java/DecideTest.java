ea81796aea3922e91a881c013fdfd3ed37050259
OscarIngels <oscar.ingels@gmail.com>
2022-02-02 16:33:57 +0100

    Merged main onto branch in order to merge branch onto main.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -12,23 +12,7 @@ class DecideTest {
         //assertFalse(dec.LIC0(dec.X, dec.Y, dec.NUMPOINTS,dec.LENGTH1));
 
     }
-    @Test
-    void LIC12TrueTest(){
-        Parameters parameters = new Parameters(4.0, 0, 0, 0, 0, 0,
-                0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 7,0, 0);
-        int[][] points = {{2, 2, 5, 7, 8, 11}, {2, 5, 6, 4, 8, 4}};
-        var dec = new Decide(parameters, 6, points);
-        assertTrue(dec.LIC12());
-    }
-    @Test
-    void LIC12FalseTest(){
-        Parameters parameters = new Parameters(40.0, 0, 0, 0, 0, 0,
-                0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 7,0, 0);
-        int[][] points = {{2, 2, 5, 7, 8, 11}, {2, 5, 6, 4, 8, 4}};
 
-        var dec = new Decide(parameters, 6, points);
-        assertFalse(dec.LIC12());
-    }
 
     @Test
 		void LIC4FalseTest(){
@@ -148,6 +132,22 @@ class DecideTest {
 		var dec = new Decide(parameters, 7, points);
 		assertTrue(dec.LIC11());
 	}
+	@Test
+	void LIC12TrueTest(){
+		Parameters parameters = new Parameters(4.0, 0, 0, 0, 0, 0,
+				0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 7,0, 0);
+		int[][] points = {{2, 2, 5, 7, 8, 11}, {2, 5, 6, 4, 8, 4}};
+		var dec = new Decide(parameters, 6, points);
+		assertTrue(dec.LIC12());
+	}
+	@Test
+	void LIC12FalseTest(){
+		Parameters parameters = new Parameters(40.0, 0, 0, 0, 0, 0,
+				0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 7,0, 0);
+		int[][] points = {{2, 2, 5, 7, 8, 11}, {2, 5, 6, 4, 8, 4}};
 
+		var dec = new Decide(parameters, 6, points);
+		assertFalse(dec.LIC12());
+	}
 
 }
